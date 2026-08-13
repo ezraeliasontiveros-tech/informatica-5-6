0d12a1e26e581cab80c99f47043d3385fdbbeccf
ezraeliasontiveros-tech <ezraeliasontiveros@gmail.com>
2026-08-13 17:20:53 +0000

    hello

---
+++ hello.py
@@ -0,0 +1,6 @@
+
+
+def main():
+    print("Hello, World!")
+
+    if_name__
---
+++ story.py
@@ -0,0 +1,22 @@
+def main():
+    planet = input("Planet:")
+
+    #separation
+    print("hello", planet)
+
+    #ending
+    print("hello",end=" ")
+    print(planet)
+
+    # concatenation
+    print("hello " +planet)
+
+    #formatted string
+    print(f"hello {planet}")
+
+if __name__=="__main__":
+    main()
+
+
+
+
