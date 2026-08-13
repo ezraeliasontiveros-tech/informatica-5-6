73e9513a954674cb865c67d2e38f2be1742856bb
ezraeliasontiveros-tech <ezraeliasontiveros@gmail.com>
2026-08-13 20:20:03 +0000

    hello

--- story.py
+++ story.py
@@ -1,18 +1,35 @@
 def main():
-    planet = input("Planet:")
+    # planet = input("Planet:")
 
-    #separation
-    print("hello", planet)
+    # #separation
+    # print("hello", planet)
 
-    #ending
-    print("hello",end=" ")
-    print(planet)
+    # #ending
+    # print("hello",end=" ")
+    # print(planet)
 
-    # concatenation
-    print("hello " +planet)
+    # # concatenation
+    # print("hello " +planet)
 
-    #formatted string
-    print(f"hello {planet}")
+    # #formatted string
+    # print(f"hello {planet}")
+
+    name = input("what´s your name?").title().strip()
+    color = input("tell me a color:").lower().strip()
+    adj = input("give me an adjetive:").lower().strip()
+    goal = input("a goal would like to achive:").lower().strip()
+
+    print(f"hello {name}!")
+    print()
+
+    print("this is your story:")
+    print(f"At dawn the sky turned {color}, and the air felt {adj}. I decided today I will finally {goal}.")
+    
+
+    print(f"At dawn the sky turned {color}, and the air felt {adj}. I decided today I will finally {goal}.")
+
+
+
 
 if __name__=="__main__":
     main()
