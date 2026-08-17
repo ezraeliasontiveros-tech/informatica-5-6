8c484b524276b2bb62f91640b8ad15fe19afb2c4
ezraeliasontiveros-tech <ezraeliasontiveros@gmail.com>
2026-08-17 19:40:12 +0000

    hello

---
+++ rectangle.py
@@ -0,0 +1,23 @@
+def main():
+    w = int(input("Enter the width of the rectangle:"))
+    print("O"*w)
+    print("O"*w)
+    print("O"*w)
+    print("O"*w)
+    print("O"*w)
+
+    p =(5*2) + (w*2)
+    print("Perimeter:",p)
+
+    a = (w*2)
+
+
+
+
+
+
+
+
+
+if __name__=="__main__":
+    main()
