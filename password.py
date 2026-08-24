1dbd29f3f4b5b838026a4314f309b7e0b06d50e7
ezraeliasontiveros-tech <ezraeliasontiveros@gmail.com>
2026-08-24 19:32:07 +0000

    hello

---
+++ password.py
@@ -0,0 +1,16 @@
+def main():
+    password = "1234"
+    atempt = input("Enter password:")
+    if password == atempt:
+        print("Correct password")
+
+    print("See you later.")
+
+
+
+
+
+
+
+if __name__=="__main__":
+    main()
