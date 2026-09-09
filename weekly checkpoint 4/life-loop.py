d088837b584cf0eef12879934be10f67824b4ed2
ezraeliasontiveros-tech <ezraeliasontiveros@gmail.com>
2026-09-09 21:40:08 +0000

    hello

--- weekly checkpoint 4/life-loop.py
+++ weekly checkpoint 4/life-loop.py
@@ -24,62 +24,62 @@ def main():
 
     while packed != 5:
 
-        print("Items you still need:")
-        if phone == "no":
-            print("--phone")
-        if backpack == "no":
-            print("--backpack")
-        if homework == "no":
-            print("--homework")
-        if water == "no":
-            print("--water")
-        if keys == "no":
-            print("--keys")
+            print("Items you still need:")
+            if phone == "no":
+                print("--phone")
+            if backpack == "no":
+                print("--backpack")
+            if homework == "no":
+                print("--homework")
+            if water == "no":
+                print("--water")
+            if keys == "no":
+                print("--keys")
 
-    print("")
+            print("")
 
     choice = input("What did you pack? ").strip().title()
     if choice == "Phone":
-    if phone == "no":
-    phone = "yes"
-    packed = packed + 1
-    print("Phone Packed")
-    else:
-    print("Phone is already packed")
+        if phone == "no":
+                phone = "yes"
+                packed = packed + 1
+                print("Phone Packed")
+        else:
+                print("Phone is already packed")
     elif choice == "Backpack":
-    if backpack == "no":
-    backpack = "yes"
-    packed = packed + 1
-    print("Backpack is packed")
-    else:
-    print("Backpack is already packed")
+            if backpack == "no":
+                backpack = "yes"
+                packed = packed + 1
+                print("Backpack is packed")
+            else:
+                print("Backpack is already packed")
     elif choice == "Homework":
-    if homework == "no":
-    homework = "yes"
-    packed = packed + 1
-    print("Homework is packed")
-    else:
-    print("Homework is already packed")
+            if homework == "no":
+                homework = "yes"
+                packed = packed + 1
+                print("Homework is packed")
+            else:
+                print("Homework is already packed")
     elif choice == "Water":
-    if water == "no":
-    water = "yes"
-    packed = packed + 1
-    print("Water is packed")
-    else:
-    print("Water is already packed")
+            if water == "no":
+                water = "yes"
+                packed = packed + 1
+                print("Water is packed")
+            else:
+                print("Water is already packed")
     elif choice == "Keys":
-    if keys == "no":
-    keys = "yes"
-    packed = packed + 1
-    print("keys are packed")
-    else:
-    print("Keys are already packed")
+            if keys == "no":
+                keys = "yes"
+                packed = packed + 1
+                print("keys are packed")
+            else:
+                print("Keys are already packed")
     elif choice == "":
-    print("Please enter an item name.")
+            print("Please enter an item name.")
     else:
-    print("That is not on yor list")
+            print("That is not on yor list")
     if packed == 5:
-    break
+        break
     print(" ")
 
     print("Checking your backpack...")
@@ -95,5 +95,5 @@ print("================================")
 
 
 if __name__ == "__main__":
-main()
+    main()
 
